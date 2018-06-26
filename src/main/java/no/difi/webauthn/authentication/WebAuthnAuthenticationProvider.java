0c1803f7eadf3805cf69e94291f8e312ad63398d
Itland <camp-vit@DIFI.no>
2018-06-26 14:40:07 +0200

    Add WebAuthnAuthenticationProvider skeleton
    The WebAuthnAuthenticationProvider implements the AuthenticationProvider
    interface, meaning that it's core to the authentication protocol of
    WebAuthn, and will essentially be a defining implementation of this
    protocol.
    
    Currently, none of the methods are really implemented, and all the
    required methods just throw NotImplementedExceptions.

---
+++ src/main/java/no/difi/webauthn/authentication/WebAuthnAuthenticationProvider.java
@@ -0,0 +1,28 @@
+package no.difi.webauthn.authentication;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AuthenticationProvider;
+
+import no.difi.webauthn.exception.*;
+
+/**
+ * This is the backbone class of WebAuthn authentication.
+ *
+ * This class implements the <code>authenticate()</code> method, which is used
+ * by the authentication manager authenticate a user based on the WebAuthn
+ * authentication protocol.
+ */
+public class WebAuthnAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        // TODO: implement
+        throw new NotImplementedException("Not yet implemented: authenticate");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        // TODO: implement
+        throw new NotImplementedException("Not yet implemented: supports");
+    }
+}
