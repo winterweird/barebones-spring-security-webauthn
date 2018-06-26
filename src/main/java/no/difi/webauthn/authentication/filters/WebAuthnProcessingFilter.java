6bfd4de2df31fe4eef6992d7dd83faca931ae453
Itland <camp-vit@DIFI.no>
2018-06-26 15:50:09 +0200

    Implement attemptAuthentication partly
    Will only ever attempt to authenticate using
    FirstOfMultiFactorAuthenticationToken requests, but can be extended to
    also allow for using a WebAuthnAuthenticationToken (probably) later.
    
    Since the WebAuthnFirstOfMultiFactorAuthenticationProvider authenticate
    method is not implemented yet, this authentication attempt will fail
    anyway.

--- src/main/java/no/difi/webauthn/authentication/filters/WebAuthnProcessingFilter.java
+++ src/main/java/no/difi/webauthn/authentication/filters/WebAuthnProcessingFilter.java
@@ -4,8 +4,18 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.util.Assert;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.util.List;
+
+import no.difi.webauthn.exception.*;
+import no.difi.webauthn.authentication.tokens.FirstOfMultiFactorAuthenticationToken;
+import no.difi.webauthn.authentication.providers.WebAuthnFirstOfMultiFactorAuthenticationProvider;
 
 /**
  * This is the class that coordinates the application of the WebAuthn login
@@ -42,5 +52,41 @@ public class WebAuthnProcessingFilter extends UsernamePasswordAuthenticationFilt
         super();
         Assert.notNull(authorities, "authorities must be set");
         this.authorities = authorities;
+    }
+
+    /**
+     * Attempt to authenticate by deciding whether to accept the
+     * username/password authentication method or to use the WebAuthn
+     * authentication protocol.
+     */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        // TODO: postOnly flag check (ignored for simplicity)
+
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+        
+        // NOTE: in a production environment, DON'T do this
+        System.out.println("USERNAME: " + username);
+        System.out.println("PASSWORD: " + password);
+
+        // TODO: retrieve the params from the request (inline)
+        // TODO: create constants for the names of the fields of the request
+        // parameters
+        String credentialId = null; // this must be a variable
+
+        AbstractAuthenticationToken authRequest;
+        if (StringUtils.isEmpty(credentialId)) {
+            authRequest = new FirstOfMultiFactorAuthenticationToken(username, password, authorities);
+        }
+        else {
+            throw new NotImplementedException("Not implemented: attemptAuthentication");
+        }
+
+        // Allow subclasses to set the "details" property
+        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
+        //                     ^ probably a protected field in some superclass
+        
+        return getAuthenticationManager().authenticate(authRequest);
     }
 }
