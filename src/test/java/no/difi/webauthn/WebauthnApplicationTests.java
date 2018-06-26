c6f27c084344d4a888ec13976e2a0e65e046bdcb
Itland <camp-vit@DIFI.no>
2018-06-26 09:17:15 +0200

    Add tests file
    Currently no tests

---
+++ src/test/java/no/difi/webauthn/WebauthnApplicationTests.java
@@ -0,0 +1,16 @@
+package no.difi.webauthn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebauthnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
